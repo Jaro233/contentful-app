b3feb4207fdda8ddc81ac350c77952e5ab7935cf
Jaro233 <jaroslaw.waliczek@vp.pl>
2023-09-19 16:17:24 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    title: "birthday buddy",
+    url: "https://birthday-buddy-by-j4ro.netlify.app/",
+    image: "./assets/birthday.png",
+  },
+  {
+    title: "tours",
+    url: "https://tours-by-j4ro.netlify.app/",
+    image: "./assets/tours.png",
+  },
+  {
+    title: "reviews",
+    url: "https://reviews-by-j4ro.netlify.app/",
+    image: "./assets/reviews.png",
+  },
+  {
+    title: "questions",
+    url: "https://accordion-by-j4ro.netlify.app/",
+    image: "./assets/questions.png",
+  },
+];
